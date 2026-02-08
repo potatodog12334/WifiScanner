b1ac7860617bd2922ba9633947a039f68712d9f6
potatodog12334 <118565455+potatodog12334@users.noreply.github.com>
2026-02-08 16:56:43 +0000

    Refactor MITRE mapping into discovery_metadata function

--- mitre.py
+++ mitre.py
@@ -1,7 +1,8 @@
-MITRE_MAPPING = {
-    "tactic": "Discovery",
-    "techniques": {
-        "T1018": "Remote System Discovery",
-        "T1046": "Network Service Discovery"
+def discovery_metadata():
+    return {
+        "tactic": "Discovery",
+        "techniques": {
+            "T1018": "Remote System Discovery",
+            "T1046": "Network Service Discovery"
+        }
     }
-}
