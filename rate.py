2ffb0725ad8bb2ed06165d8f5812477dbc2d8022
potatodog12334 <118565455+potatodog12334@users.noreply.github.com>
2026-02-08 16:55:11 +0000

    Refactor RateLimiter to rate_limit function

--- rate.py
+++ rate.py
@@ -1,9 +1,5 @@
 import time
 
-class RateLimiter:
-    def __init__(self, delay):
-        self.delay = delay
-
-    def wait(self):
-        if self.delay > 0:
-            time.sleep(self.delay)
+def rate_limit(seconds):
+    if seconds > 0:
+        time.sleep(seconds)
