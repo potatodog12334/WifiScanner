992f4f1bc3b392807279e19286f03ac963523f04
potatodog12334 <118565455+potatodog12334@users.noreply.github.com>
2026-02-08 16:56:14 +0000

    Implement passive_discovery stub function
    Added a stub function for future passive discovery techniques.

--- passive.py
+++ passive.py
@@ -1,23 +1,5 @@
-import subprocess
-
-def passive_hosts():
-    hosts = {}
-
-    try:
-        output = subprocess.check_output(
-            ["arp", "-a"],
-            stderr=subprocess.DEVNULL
-        ).decode(errors="ignore")
-
-        for line in output.splitlines():
-            if "(" in line and ")" in line:
-                ip = line.split("(")[1].split(")")[0]
-                hosts[ip] = {
-                    "source": "passive",
-                    "hostname": None,
-                    "ports": []
-                }
-    except:
-        pass
-
-    return hosts
+def passive_discovery():
+    """
+    Stub for future passive techniques (ARP cache, etc).
+    """
+    return {}
