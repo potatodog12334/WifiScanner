c7ab25172b29570e52a8d4005297bb9c993d6682
potatodog12334 <118565455+potatodog12334@users.noreply.github.com>
2026-02-08 16:57:25 +0000

    Remove emit function and add print_json function

--- output.py
+++ output.py
@@ -1,17 +1,4 @@
 import json
-from datetime import datetime
-from mitre import MITRE_MAPPING
 
-def emit(hosts, engagement, window, rate):
-    output = {
-        "metadata": {
-            "engagement": engagement,
-            "timestamp": datetime.utcnow().isoformat() + "Z",
-            "scan_window": window,
-            "rate_limit_seconds": rate,
-            "mitre_attack": MITRE_MAPPING
-        },
-        "hosts": hosts
-    }
-
-    print(json.dumps(output, indent=2))
+def print_json(data):
+    print(json.dumps(data, indent=2))
